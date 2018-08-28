fd20a17a56937b95497174f8b889cb0791f6d782
Tsvetoslav <tsvetoslav.tsvetkov@gmail.com>
2018-08-28 16:08:04 +0300

    Add mockito-demo

---
+++ mockito-demo/src/main/java/org/vexelon/net/calculatorservice/CalculatorService.java
@@ -0,0 +1,11 @@
+package org.vexelon.net.calculatorservice;
+
+public interface CalculatorService {
+	public double add(double input1, double input2);
+
+	public double subtract(double input1, double input2);
+
+	public double multiply(double input1, double input2);
+
+	public double divide(double input1, double input2);
+}
---
+++ mockito-demo/src/main/java/org/vexelon/net/stockservice/StockService.java
@@ -0,0 +1,5 @@
+package org.vexelon.net.stockservice;
+
+public interface StockService {
+	   public double getPrice(Stock stock);
+}
